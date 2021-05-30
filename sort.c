fcc805c17b364e6564ead785a4cb06fa9fcfa118
kittim1000 <84755321+kittim1000@users.noreply.github.com>
2021-05-30 16:36:21 -0400

    Update sort.c

--- sort.c
+++ sort.c
@@ -1,97 +1,123 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #define LMAX 100
 #define NMAX 200
 
-struct raws {
-  char date[NMAX]; char ddd[NMAX]; char time[NMAX]; char cscore[NMAX];
-  char fof2[NMAX][NMAX]; char fof1[NMAX]; char foe[NMAX]; char foes[NMAX];
-  char hes[NMAX]; char hmf2[NMAX]; char hmf1[NMAX]; char hme[NMAX];
-  char b0[NMAX]; char b1[NMAX]; char d1[NMAX];
-};
-
 void medfilter(double *, int);
 void swap (double*, double*);
-int compare_raws(struct raws *, struct raws *);
+int compare_raws(const void *, const void *);
+
+struct raws {
+  int yy, mm, dd, ddd, hh, mn, ss, cscore;
+  double fof2, fof1, foe;
+  char foes[3], hmes[3];
+  double hmf2, hmf1, hme, b0, b1;
+};
 
 void main() {
 
   struct raws line[LMAX], tmp;
 
-  int i, imax;
+  int i, ii, imax, j, c1, c2;
   char str[NMAX], header[NMAX];
   double f_fof2[LMAX], f_hmf2[LMAX];
 
   i=0;
+  ii=1;
   while ( fgets (str, NMAX, stdin) != NULL) {
+
     if (str[0] == '#') { strcpy(header, str); }
     else {
-      sscanf(str, "%s %s %s %s %s %s %s %s %s %s %s %s %s %s",
-        tmp.date, tmp.ddd, tmp.time, tmp.cscore,
-        tmp.fof2, tmp.fof1, tmp.foe, tmp.foes, tmp.hes, tmp.hmf2, tmp.hmf1, tmp.hme, tmp.b0, tmp.b1, tmp.d1);
+      c1 = sscanf(str, "%d.%d.%d (%d) %d:%d:%d %d %lf %lf %lf %s %s %lf %lf %lf %lf %lf",
+        &tmp.yy, &tmp.mm, &tmp.dd, &tmp.ddd, &tmp.hh, &tmp.mn, &tmp.ss, &tmp.cscore,
+        &tmp.fof2, &tmp.fof1, &tmp.foe, tmp.foes, tmp.hmes, &tmp.hmf2, &tmp.hmf1, &tmp.hme, &tmp.b0, &tmp.b1);
 
-      line[i] = tmp;
-      i++;
+      c2 = 1;
+      for (j=0; j<i; j++) {
+        if (compare_raws(&line[j], &tmp) == 0) {
+          c2 = 0;
+          break;
+        }
+      }
+
+      if (c1 < 18) {
+        printf("[Missing column] line %d: %s", ii, str);
+      }
+      else if (c2 == 0) {
+        printf("[Duplicate line] line %d: %s", ii, str);
+      }
+      else {
+        line[i] = tmp;
+        i++;
+      }
     }
+    ii++;
   }
   imax = i;
 
-  qsort(line, imax, sizeof(line[0]), compare_raws);
+  qsort(line, imax, sizeof(struct raws), compare_raws);
 
-  printf(header);
+  printf("\n%s", header);
   for(i=0; i<imax; i++) {
     tmp = line[i];
-    printf("%s %s %s %s %s %s %s %s %s %s %s %s %s %s\n",
-      tmp.date, tmp.ddd, tmp.time, tmp.cscore,
-      tmp.fof2, tmp.fof1, tmp.foe, tmp.foes, tmp.hes, tmp.hmf2,
-      tmp.hmf1, tmp.hme, tmp.b0, tmp.b1, tmp.d1);
+
+    printf("%d.%2d.%2d (%3d) %2d:%2d:%2d %3d %.3f %.3f %.3f %s %s %.3f %.3f %.3f %.3f %.3f\n",
+      tmp.yy, tmp.mm, tmp.dd, tmp.ddd, tmp.hh, tmp.mn, tmp.ss, tmp.cscore,
+      tmp.fof2, tmp.fof1, tmp.foe, tmp.foes, tmp.hmes, tmp.hmf2, tmp.hmf1, tmp.hme, tmp.b0, tmp.b1);
   }
   printf("\n");
 
 // Median Filter
 
   for (i=0; i<imax; i++) {
-    f_fof2[i] = atof( line[i].fof2 );
-    f_hmf2[i] = atof( line[i].hmf2 );
+    f_fof2[i] = line[i].fof2;
+    f_hmf2[i] = line[i].hmf2;
   }
 
   medfilter(f_fof2, imax);
-  printf("Median Filter, foF2: \n");
-  for (i=0; i<imax; i++) { printf ("%5.3f\n", f_fof2[i]); } printf ("\n");
-
   medfilter(f_hmf2, imax);
-  printf("Median Filter, hmf2: \n");
-  for (i=0; i<imax; i++) { printf ("%5.3f\n", f_hmf2[i]); } printf ("\n");
 
+  printf("Median Filter, foF2 & hmf2: \n");
+
+  for (i=0; i<imax; i++) {
+    tmp = line[i];
+
+    printf("%d.%2d.%2d (%3d) %2d:%2d:%2d %.3f %.3f\n",
+      tmp.yy, tmp.mm, tmp.dd, tmp.ddd, tmp.hh, tmp.mn, tmp.ss, f_fof2[i], f_hmf2[i]);
+  }
 }
 
-int compare_raws(struct raws *s, struct raws *t) {
+int compare_raws(const void *s0, const void *t0) {
   int i;
+  struct raws *s, *t;
 
-  if ( (i = strcmp(s->date, t->date)) !=0 ) { return i; }
+  s = (struct raws*) s0;
+  t = (struct raws*) t0;
+
+  if ((i = (s->yy)-(t->yy)) != 0) { return i; }
   else {
-    return strcmp(s->time, t->time);
+    i = 86400*((s->ddd)-(t->ddd)) + 3600*((s->hh)-(t->hh)) + 60*((s->mn)-(t->mn)) + (s->ss)-(t->ss);
+    return i;
   }
 }
 
 void medfilter(double v[], int n) {
   int i;
-  double tmp[n], a, b, c;
+  double temp[n], a, b, c;
 
-  tmp[0] = (v[0]+v[1])/2;
-  tmp[n-1] = (v[n-2]+v[n-1])/2;
+  temp[0] = (v[0]+v[1])/2;
+  temp[n-1] = (v[n-2]+v[n-1])/2;
 
   for (i=1; i<n-1; i++) {
     a = v[i-1]; b = v[i]; c = v[i+1];
     if (a<b) { swap(&a,&b); }
     if (a<c) { swap(&a,&c); }
     if (b<c) { swap(&b,&c); }
-    tmp[i] = b;
+    temp[i] = b;
   }
 
-  for (i=0; i<n; i++) { v[i] = tmp[i]; }
+  for (i=0; i<n; i++) { v[i] = temp[i]; }
 }
 
 void swap(double* a, double* b) {
